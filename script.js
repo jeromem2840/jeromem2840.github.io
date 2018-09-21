ea0aca008d37c28eacd600b3db107f6f8a4f4829
MLG Jerri <43275596+jeromem2840@users.noreply.github.com>
2018-09-21 11:44:02 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+alert("Te MLG is Here");
+
+function dropdown() {
+	var x = document.getElementsByClassName("dropdown-content");
+	for (var i = 0; i < x.length; i++) {
+		if (x[i].style.display === "none") { 
+			x[i].style.display = "block";
+		}
+		else {
+			x[i].style.display = "none";
+		}
+	}
+}
